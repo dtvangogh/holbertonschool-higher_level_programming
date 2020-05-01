637d05dd8765691d9765e6e9613476fc746c2fe6
dt <dtvangogh@github.com>
2020-04-30 23:23:38 -0400

    infite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total = 0
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        total += int(sys.argv[i])
+    print("{}".format(total))
