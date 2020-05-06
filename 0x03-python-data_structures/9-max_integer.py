d99b69b58a20016adcc24c3ded4ea82c92abc217
dt <dtvangogh@github.com>
2020-05-05 23:53:42 -0400

    find max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(list=[]):
+    if len(list) == 0:
+        return None
+    max_val = list[0]
+    if list:
+        for i in list:
+            if i > max_val:
+            max_val = i
+        return max_val
