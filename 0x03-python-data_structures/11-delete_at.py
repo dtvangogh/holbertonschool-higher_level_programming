3a407d79d3f3a3fb672f4f1e3f1b27e993671b8a
dt <dtvangogh@github.com>
2020-05-05 23:58:25 -0400

    delete idnex

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(list=[], idx=0):
+    if idx < 0:
+        return list
+    if idx >= len(list):
+        return list
+    else:
+        del list[idx]
+        return list
