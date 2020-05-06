9335d23f829ab5d2dcd8aa750648e828feccd8a1
dt <dtvangogh@github.com>
2020-05-05 23:49:26 -0400

    replace prototype

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def remove_Cc(my_string):
+def no_c(my_string):
     new_string = ""
     for i in range(0, len(my_string)):
         if my_string[i] is not 'c' and my_string[i] is not 'C':
