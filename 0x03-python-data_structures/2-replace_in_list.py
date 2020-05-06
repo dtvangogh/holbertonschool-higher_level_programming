376d1ad81f48f53b613bc720769732bb5bd3cafb
dt <dtvangogh@github.com>
2020-05-05 23:28:54 -0400

    3 cases

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(list, idx, element):
+    if idx < 0:
+        return list
+    if idx >= len(list):
+        return list
+    else:
+        list[idx] = element
+        return list
