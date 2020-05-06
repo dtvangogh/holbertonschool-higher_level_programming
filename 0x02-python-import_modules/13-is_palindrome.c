964e7e22f05e38b308c2f77416a64f17b6705da4
dt <dtvangogh@github.com>
2020-05-06 10:52:32 -0400

    mit

--- 0x02-python-import_modules/13-is_palindrome.c
+++ 0x02-python-import_modules/13-is_palindrome.c
@@ -1,31 +1,28 @@
 #include "lists.h"
-
-/**
- * is_palindrome - is the list a palindrome
- * @head: head of the list
- * Return: 1 if True 0 if False
- */
+#include <stdlib.h>
 
 int is_palindrome(listint_t **head)
 {
-	int size = 0;
-	int nums[2048];
+	int size;
+	int options[2048];
 	int i;
 
-	if (*head == NULL)
+	size = 0;
+	if (head == NULL || *head == NULL)
 		return (1);
 
 	while (*head != NULL)
 	{
 		size++;
-		nums[size - 1] = (*head)->n;
+		options[size - 1] = (*head)->n;
 		head = &(*head)->next;
 	}
 
 	for (i = 0; i < size / 2; i++)
 	{
-		if (nums[i] != nums[size - i - 1])
+		if (options[i] != options[size - i - 1])
 			return (0);
 	}
+
 	return (1);
 }
