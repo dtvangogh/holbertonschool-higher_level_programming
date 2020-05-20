28af962a45d04cee09ce92f1a8d7c30d63a73e48
dt <dtvangogh@github.com>
2020-05-19 22:57:19 -0400

    chanage order of error statements

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,8 +9,8 @@ class Square:
 
     def __init__(self, size=0):
         """set size and create square instance"""
-        if size < 0:
-            raise ValueError('size must be >= 0')
         if type(size) is not int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
