c5c34f50ac8f78f3e6c3e6780ece654ae5c8480a
dt <dtvangogh@github.com>
2020-05-19 22:53:41 -0400

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+ Square Class: contains size variable
+"""
+
+
+class Square:
+    """ creates a private size variable """
+    def __init__(self, size):
+        """ initialize variable """
+        self.__size = size
