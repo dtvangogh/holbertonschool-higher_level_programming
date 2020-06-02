73b9fdeb422da45b9367be4ab4aa12de9edbb02f
dt <dtvangogh@github.com>
2020-06-02 17:42:33 -0600

    isinstance builtin

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" functions check if object the class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ test if instance of the class """
+    return isinstance(obj, a_class)
