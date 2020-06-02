ccea6161e6534481c80219045ca2a5917171ce54
dt <dtvangogh@github.com>
2020-06-02 17:58:58 -0600

    single exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Geometry class
+"""
+
+
+class BaseGeometry:
+    """ Does calculations """
+    def area(self):
+        """ area """
+        raise Exception("area() is not implemented")
