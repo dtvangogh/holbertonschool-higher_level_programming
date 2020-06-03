7d9f636858608270819b5e2e45414eb1480c8042
dt <dtvangogh@github.com>
2020-06-03 15:43:37 -0700

    fix attributes in function def to json

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -14,7 +14,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Returns a dictionary representation of init
         """
 
