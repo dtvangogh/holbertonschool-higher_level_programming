bd42bcf4fb1192201306e06969e3df07a247276a
dt <dtvangogh@github.com>
2020-06-03 10:43:35 -0700

    append

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Append to file
+"""
+
+
+def append_write(filename="", text=""):
+    """ Append to file """
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
