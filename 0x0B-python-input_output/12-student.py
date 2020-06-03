300f311246148566f61bd67b36c9695fc1425a2a
dt <dtvangogh@github.com>
2020-06-03 15:41:33 -0700

    retrieve if conditiosn met

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+
+    """The Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation function for Student class
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of init
+        """
+
+        if not isinstance(attrs, list):
+            return self.__dict__
+        for var in attrs:
+            if not isinstance(var, str):
+                return self.__dict__
+        string_dict = {}
+        for string in attrs:
+            if string in self.__dict__.keys():
+                string_dict[string] = self.__dict__[string]
+        return string_dict
