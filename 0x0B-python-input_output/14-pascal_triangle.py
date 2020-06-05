5bf6b11bd7e9399d9527f844cd1e81dc3725e1c6
dt <dtvangogh@github.com>
2020-06-04 22:32:27 -0700

    pep8 remove blank line at end of file

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -10,18 +10,15 @@ def pascal_triangle(n):
         return [[1]]
     if n == 2:
         return [[1], [1, 1]]
-    oldlist=[[1], [1, 1]]
+    oldlist = [[1], [1, 1]]
     m = n - 1
     previous_list = [1, 1, 1]
     for x in range(2, n):
         newlist = [1 for i in range(0, n - m + 2)]
         for y in range(1, n - m + 1):
-            newlist[y] = previous_list[0 + y - 1] + previous_list[1 + y -1]
+            newlist[y] = previous_list[0 + y - 1] + previous_list[1 + y - 1]
         previous_list = newlist
         oldlist.append(newlist)
         m = m - 1
 
     return oldlist
-
-
-
