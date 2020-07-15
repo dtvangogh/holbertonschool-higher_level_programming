8452f1f8f731ee7a248aafd2cf78a375327dd9e6
dtvangogh <dt.van16@gmail.com>
2020-07-14 23:59:06 -0400

    all

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- lists all privledge for user_01 and user_02
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW CRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+--  lists all the cities of state id 1
+SELECT id, name FROM cities WHERE `state_id` = 1 ORDER by id;
