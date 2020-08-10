85d0ef1bd908f58f02bbb80bfe5679b5c54648e3
dt <dtvangogh>
2020-08-10 06:07:18 +0000

    add

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
-no prototype
+"""Start link class to table in database 
+"""
+import sys
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    arg = argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    """why does .first work but .all not work? """
+    whatWeLookingAt = session.query(State).filter(State.name == arg).first() 
+    if whatWeLookingAt is not None:
+        print("{}".format(whatWeLookingAt.id))
+    else:
+        print("Not found")
+            
+        
