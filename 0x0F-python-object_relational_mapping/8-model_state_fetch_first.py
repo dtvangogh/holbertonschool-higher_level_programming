8bac72ea498b5ebb7be48a7853f84b91d3b7dfb0
dt <dtvangogh>
2020-08-10 05:24:42 +0000

    add

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
-no prototype
+"""Start link class to table in database 
+"""
+import sys
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    whatWeLookingAt = session.query(State).order_by(State.id.asc()).first() 
+    if whatWeLookingAt is None:
+    	print("Nothing")
+    else:
+        print("{}: {}".format(whatWeLookingAt.id, whatWeLookingAt.name))
+    session.close()
