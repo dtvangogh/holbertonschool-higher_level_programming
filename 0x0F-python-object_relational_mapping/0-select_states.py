20e410909c543d818c95683055b486398f0a39a7
dt <dtvangogh@github.com>
2020-08-10 02:27:34 +0000

    add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-no prototype
+"""
+Script will display states sorted by id
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
