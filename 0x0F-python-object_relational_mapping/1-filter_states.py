2bd1f6b8939dfbe51e5a9e86ca52315fb140fb92
dt <dtvangogh@github.com>
2020-08-10 03:00:54 +0000

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
-no prototype
+"""
+Script will display states started with capital N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
