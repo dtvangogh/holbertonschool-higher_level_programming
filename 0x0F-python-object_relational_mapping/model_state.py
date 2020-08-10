3294792a24f48f27ccd8b079e8b8bf889ba17d30
dt <dtvangogh>
2020-08-10 04:42:21 +0000

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-no prototype
+"""
+State class
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
