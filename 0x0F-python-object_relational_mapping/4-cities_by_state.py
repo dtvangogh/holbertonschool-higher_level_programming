b884a66668d72b0089e2080b4e31045a9df079e1
dt <dtvangogh>
2020-08-10 03:41:21 +0000

    add

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
-no prototype
+"""
+Script will display states started with capital N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+
+    cursor = db.cursor()
+    selection = "SELECT cities.id, cities.name, states.name \
+              FROM cities LEFT JOIN states \
+              ON cities.state_id = states.id \
+              ORDER BY id ASC;"
+    cursor.execute(selection)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
