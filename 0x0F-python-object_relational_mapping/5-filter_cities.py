ae00267a78f6ddc1cbe1d1811193964668b2e4d6
dtvangogh <dt.van16@gmail.com>
2020-08-15 02:27:52 -0400

    pep8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     selection = "SELECT cities.id, cities.name, states.name \
               FROM cities LEFT JOIN states \
               ON cities.state_id = states.id \
-	      WHERE states.name='{}' \
+          WHERE states.name='{}' \
               ORDER BY id ASC".format(state)
     cursor.execute(selection)
     rows = cursor.fetchall()
