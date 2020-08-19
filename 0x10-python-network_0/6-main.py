9d5d34032c7a82ab7306fece4aa15e9faa6b76b8
dtvangogh <dt.van16@gmail.com>
2020-08-19 02:01:21 -0400

    add

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,4 +1,3 @@
-guillaume@ubuntu:~/0x10$ cat 6-main.py
 #!/usr/bin/python3
 """ Test function find_peak """
 find_peak = __import__('6-peak').find_peak
@@ -9,14 +8,3 @@ print(find_peak([2, 2, 2]))
 print(find_peak([]))
 print(find_peak([-2, -4, 2, 1]))
 print(find_peak([4, 2, 1, 2, 3, 1]))
-
-guillaume@ubuntu:~/0x10$ ./6-main.py
-6
-3
-2
-None
-2
-4
-guillaume@ubuntu:~/0x10$ wc -l 6-peak.txt 
-2 6-peak.txt
-guillaume@ubuntu:~/0x10$ 
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,16 +7,6 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-    l = list_of_integers
-    if len(l) == 1:
-        return l[0]
-    i = int(len(l)/2)
-    if len(l) == 2:
-        return l[0] if l[0] > l[1] else l[1]
-    if l[i - 1] <= l[i] >= l[i + 1]:
-        return l[i]
-    else:
-        if l[i] < l[i + 1]:
-            return find_peak(l[i:])
-        else:
-            return find_peak(l[:i])
+    if list_of_integers:
+    	peak = max(list_of_integers)
+    return(peak)
