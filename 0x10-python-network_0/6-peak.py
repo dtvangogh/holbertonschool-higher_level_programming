8c0ff0d91c36d2e2467d28201afded4c2e5a4b5a
dtvangogh <dt.van16@gmail.com>
2020-08-19 02:15:04 -0400

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,6 @@
 
 def find_peak(list_of_integers):
     """find the peak of list of unsorted integers"""
-
     if list_of_integers is None or list_of_integers == []:
         return None
     if list_of_integers:
