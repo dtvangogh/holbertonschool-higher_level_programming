f068a3f71f792a2713e7f8709ba3a5ce2bd7a419
dtvangogh <dt.van16@gmail.com>
2020-08-19 02:17:38 -0400

    pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,5 +7,5 @@ def find_peak(list_of_integers):
     if list_of_integers is None or list_of_integers == []:
         return None
     if list_of_integers:
-    	peak = max(list_of_integers)
+        peak = max(list_of_integers)
     return(peak)
