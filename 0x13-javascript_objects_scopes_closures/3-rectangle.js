862586527c4c94660b615ec6a558e0a9f9cb007f
dt <dtvangogh@github.com>
2020-09-21 20:39:06 +0000

    semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,10 +4,11 @@ module.exports =
     class Rectangle {
       constructor (w, h) {
         if (h > 0 && w > 0) {
-        this.width = w;
-        this.height = h;
-        } 
+          this.width = w;
+          this.height = h;
+        }
       }
+
       print () {
         for (let i = 0; i < this.height; i++) {
           console.log('X'.repeat(this.width));
