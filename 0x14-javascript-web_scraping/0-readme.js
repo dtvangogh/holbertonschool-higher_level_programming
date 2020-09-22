056aee17023b51193f5791b0b4778f369d41bfac
dt <dtvangogh@github.com>
2020-09-22 21:36:09 +0000

    semistandrad

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// Reads and prints a file
 
-const arguments = process.argv;
-let fs = require('fs');
-fs.readFile(arguments[2], 'utf8', function (err, data) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
