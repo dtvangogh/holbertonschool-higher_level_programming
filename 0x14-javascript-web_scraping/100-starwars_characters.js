ea2e68956b5a44a8468603595f84b22e2210001a
dtvangogh <dt.van16@gmail.com>
2020-09-22 21:11:29 -0400

    add

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 request('http://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
-  const characters = JSON.parse(body).characters
+  const characters = JSON.parse(body).characters;
   if (err) throw err;
-  for (let character of characters) {
+  for (const character of characters) {
     request(character, function (err, response, body) {
       if (err) throw err;
       console.log(JSON.parse(body).name);
