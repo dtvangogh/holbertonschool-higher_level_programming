ababc4f7901b85bb907ca561d56a58b19cdf7a80
dtvangogh <dt.van16@gmail.com>
2020-09-25 12:00:08 -0400

    add

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  const movies = data.results;
-  movies.forEach((movie) => {
-    $('<li>' + movie.title + '</li>').appendTo('UL#list_movies');
+  data.results.forEach((movie) => {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
   });
 });
